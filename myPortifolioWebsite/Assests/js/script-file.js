c96206ee64930bd987a5c92f783c35e02be947cd
markgoli <golimark3@gmail.com>
2023-08-22 16:09:35 +0300

    my portfolio website done

--- myPortifolioWebsite/Assests/js/script-file.js
+++ myPortifolioWebsite/Assests/js/script-file.js
@@ -13,3 +13,40 @@ document.addEventListener("DOMContentLoaded", function() {
         textElement.classList.remove("animate-fade-in");
     });
 });
+
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function Opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link');
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add('active-link')
+    document.getElementById(tabname).classList.add('active-tab');
+    
+}
+
+// var navlinks = document.getElementsByClassName('active-nav-tab');
+
+// function ActivatePage() {
+//     for(navlink of navlinks){
+//         navlink.classList.remove('active-nav-link');
+//     }
+//     event.currentTarget.classList.add('active-nav-link');
+// }
+
+
+
+var sidemenu = document.getElementById('sidemenu');
+function openmenu(){
+    sidemenu.style.right = '0';
+}
+
+function closemenu(){
+    sidemenu.style.right = '-200px';
+}
